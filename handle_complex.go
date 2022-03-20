e8ef0fd67a4db20bac199e71374d83f83cc717a6
Sascha Winkelhofer <sascha.winkelhofer@arivum.de>
2022-03-20 19:41:49 +0100

    added license

--- handle_complex.go
+++ handle_complex.go
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2022, arivum.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 package json2msgpackStreamer
 
 func (t *JSON2MsgPackStreamer) handleArray() error {
